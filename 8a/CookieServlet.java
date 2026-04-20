ca89c98ac41fe39d273111dc145f4f28c5c75483
AshthoshN <4al24cs038@aiet.org.in>
2026-04-20 21:55:16 +0530

    Add files via upload

---
+++ 8a/CookieServlet.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class CookieServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("username");
+
+        int visitCount = 1;
+
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("visitCount")) {
+                    visitCount = Integer.parseInt(c.getValue()) + 1;
+                }
+                if (c.getName().equals("username")) {
+                    name = c.getValue();
+                }
+            }
+        }
+
+        // Create cookies
+        Cookie nameCookie = new Cookie("username", name);
+        Cookie countCookie = new Cookie("visitCount", String.valueOf(visitCount));
+
+        // Expiry (30 seconds for demo)
+        nameCookie.setMaxAge(30);
+        countCookie.setMaxAge(30);
+
+        response.addCookie(nameCookie);
+        response.addCookie(countCookie);
+
+        // Output
+        out.println("<h2>Welcome back " + name + "!</h2>");
+        out.println("<h3>You have visited this page " + visitCount + " times</h3>");
+
+        out.println("<h3>Cookie List:</h3>");
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                out.println("Name: " + c.getName() + "<br>");
+                out.println("Value: " + c.getValue() + "<br>");
+                out.println("Max Age: " + c.getMaxAge() + "<br>");
+                out.println("----------------------<br>");
+            }
+        }
+
+        out.println("<br><p>Note: Cookies will expire in 30 seconds.</p>");
+    }
+}
